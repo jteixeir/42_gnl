ad1fbbbe9f666f4e8211842e6e8fa6c8cbe308bd
jteixeir <jessica.tsousa@gmail.com>
2020-12-13 18:52:11 -0300

    GNL - Understanding static variables

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # 42_gnl
 42 école project
 
+
+1) Open a file and discover check if there is a valid file descriptor
+2) Read the file according to the defined BUFFER
+3) Course the chars readen until you find a line breaker '\n'
+4) If you find a return 1 as "A line has been read"
+5) If you don't find keep looking
+
--- get_next_line.c
+++ get_next_line.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jteixeir <jteixeir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jessica <jessica@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 12:01:14 by jteixeir          #+#    #+#             */
-/*   Updated: 2020/09/05 20:55:33 by jteixeir         ###   ########.fr       */
+/*   Updated: 2020/12/13 18:50:23 by jessica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 
 #ifndef BUFFER_SIZE
@@ -19,7 +20,41 @@
 #endif
 
 
+int a_line_exists(char *s)
+{
+	int i;
 
+	i = 0;
+
+	while(s[i])
+	{
+		if(s[i] == '\n')
+		{
+			printf("A line has been found\nThe position is %d\n", i);
+			return(1);
+		}
+		i++;
+	}
+	return(0);
+}
+
+int reading_file(int fd)
+{
+	int read_return;
+	char *buffer;
+
+	buffer = (char*)malloc(BUFFER_SIZE + 1);
+
+	printf("This is my test file context:\n");
+	while((read_return = read(fd, buffer, BUFFER_SIZE)>0))
+	{
+		printf("%s", buffer);
+		read_return--;
+	}
+	printf("\n\n");
+	a_line_exists(buffer);
+	return(0);
+}
 
 
 
@@ -29,21 +64,8 @@ int	main(void)
 	char *file = "teste1.txt";
 
 	fd = open(file, O_RDONLY);
-	printf("%d\n", fd);
+	printf("This is my File Descriptor: %d\n\n", fd);
 
-	
-	if((fd < 0) || (BUFFER_SIZE <= 0)) //incluir o tratamento da !line
-		return(-1);
-
-	int rr;
-	char buffer[5];
-	
-	while((rr = read(fd, buffer, BUFFER_SIZE)>0))
-	{
-		printf("%s", buffer);
-		--rr;
-	}
-
-
+	reading_file(fd);
 	return (0);
 }
